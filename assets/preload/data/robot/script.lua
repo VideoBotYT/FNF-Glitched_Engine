9005b7d8a4c7449f703a26b057a9d2ba23e94ac5
GAMERMEER <113830775+GAMERMEER@users.noreply.github.com>
2022-11-09 15:57:15 +0100

    fnf-glitched-engine

---
+++ assets/preload/data/robot/script.lua
@@ -0,0 +1,6 @@
+function onSongStart()
+	noteTweenX("NoteMove1", 4, 600, 0.2, circInOut)
+    noteTweenY("NoteMove2", 5, 300, 0.2, circInOut)
+    noteTweenY("NoteMove3", 6, 10, 0.2, circInOut)
+    noteTweenX("NoteMove4", 7, 1150, 0.2, circInOut)
+end
