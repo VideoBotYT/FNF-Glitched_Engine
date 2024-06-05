5cc7a956c3c36274c0ad1eee18cfbf38acbcddef
VideoBot <113830775+VideoBotYT@users.noreply.github.com>
2024-06-05 19:26:51 +0200

    Update OutdatedState.hx

--- source/OutdatedState.hx
+++ source/OutdatedState.hx
@@ -41,7 +41,7 @@ class OutdatedState extends MusicBeatState
 		if(!leftState) {
 			if (controls.ACCEPT) {
 				leftState = true;
-				CoolUtil.browserLoad("https://github.com/ShadowMario/FNF-PsychEngine/releases");
+				CoolUtil.browserLoad("https://github.com/VideoBotYT/FNF-GLitched_Engine/releases");
 			}
 			else if(controls.BACK) {
 				leftState = true;
